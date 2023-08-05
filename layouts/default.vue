5ea81bb4dfbe5b89c52fd630ae57a1bb443b238a
iqho <hosiqb@gmail.com>
2023-08-05 22:07:08 +0600

    My First Commit

---
+++ layouts/default.vue
@@ -0,0 +1,9 @@
+<template>
+    <div>
+        <TheHeader />
+        <slot />
+
+        <TheFooter />
+    </div>
+</template>
+  
---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <h1>Iqbal Hossen</h1>
+    </div>
+</template>
+<script>
+export default {
+
+}
+</script>
+<style lang="">
+    
+</style>
